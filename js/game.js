75f18b87c587ea557b553d334d4a359573bcbe89
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-24 00:18:32 +0200

    Refactor

--- js/game.js
+++ js/game.js
@@ -2,26 +2,16 @@ function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   switch (randomNumber) {
     case 1:
-      return "Rock";
+      return "rock";
     case 2:
-      return "Paper";
+      return "paper";
     case 3:
-      return "Scissors";
+      return "scissors";
   }
 }
 
 function getHumanChoice() {
-  const number = parseInt(prompt("Choose a number between 1 and 3:"));
-  switch (number) {
-    case 1:
-      return "Rock";
-    case 2:
-      return "Paper";
-    case 3:
-      return "Scissors";
-    default:
-      getHumanChoice();
-  }
+  return prompt("Choose between rock, paper and scissors:").toLocaleLowerCase();
 }
 
 let computerScore = 0;
