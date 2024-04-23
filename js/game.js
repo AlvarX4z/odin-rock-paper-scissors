67c5a28f5a14191212af5613d0b0493b0cbd7e28
Álvaro de Francisco <alfrasan@gmail.com>
2024-04-23 23:31:45 +0200

    Implement getComputerChoice function

--- js/game.js
+++ js/game.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  switch (randomNumber) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
